f5415a1275efb091eac10de8815470204e807626
Devashish-s-Legacy <devashish.gdsc1@gmail.com>
2025-01-19 19:40:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from src.controller import pdf_controller, extraction_controller, embedding_controller, query_controller
+
+app = FastAPI()
+
+# Include routers
+app.include_router(pdf_controller.router, prefix="/api/v1")
+app.include_router(extraction_controller.router, prefix="/api/v1")
+app.include_router(embedding_controller.router, prefix="/api/v1")
+app.include_router(query_controller.router, prefix="/api/v1")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
